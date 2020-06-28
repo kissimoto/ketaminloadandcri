50892bc0dcc5071d0f10e6c44527c2f8af45d4ea
kissimoto <bmc_hit@yahoo.com>
2020-06-28 20:51:12 +0100

    Update met_vet.js

--- met_vet.js
+++ met_vet.js
@@ -1,1 +1,58 @@
-const _0xdac9=['loadingDoseRate','Ketamine\x20loading\x20dose\x20=\x20','dose_rate','getElementById','value','\x20ml','result','bagSize','log','innerHTML','<h3>Continuous\x20Rate\x20Infusion\x20Calculation\x20<br></h3>Add\x20','ml\x20to\x20the\x20fluid\x20bag<br>\x20Set\x20the\x20fluid\x20rate\x20to\x20','weight','toFixed','round'];(function(_0x594886,_0xdac9e5){const _0x1a07ba=function(_0x389592){while(--_0x389592){_0x594886['push'](_0x594886['shift']());}};_0x1a07ba(++_0xdac9e5);}(_0xdac9,0x1db));const _0x1a07=function(_0x594886,_0xdac9e5){_0x594886=_0x594886-0x0;let _0x1a07ba=_0xdac9[_0x594886];return _0x1a07ba;};function ketaprocess(){const _0x1b20ff=document[_0x1a07('0x8')](_0x1a07('0x2'))[_0x1a07('0x9')]*document[_0x1a07('0x8')](_0x1a07('0x5'))[_0x1a07('0x9')];console[_0x1a07('0xd')](_0x1b20ff);const _0x33be34=_0x1b20ff/0x64;var _0x1dfc9d=(Math[_0x1a07('0x4')](0x64*_0x33be34)/0x64)[_0x1a07('0x3')](0x3);document['getElementById']('result1')['innerHTML']=_0x1a07('0x6')+_0x1dfc9d+_0x1a07('0xa');}function process(){const _0x5f2fe9=document[_0x1a07('0x8')]('weight')['value'],_0x5d59ad=document['getElementById']('fluidRate')[_0x1a07('0x9')],_0x359b85=document[_0x1a07('0x8')](_0x1a07('0x7'))['value'],_0x24b35f=document[_0x1a07('0x8')](_0x1a07('0xc'))[_0x1a07('0x9')],_0x9d5602=_0x5f2fe9*_0x5d59ad,_0x3f8ac7=_0x359b85/_0x5d59ad;console[_0x1a07('0xd')](_0x3f8ac7);const _0x426fce=_0x3f8ac7*_0x24b35f/0x64;console[_0x1a07('0xd')](_0x426fce);document[_0x1a07('0x8')](_0x1a07('0xb'))[_0x1a07('0xe')]=_0x1a07('0x0')+_0x426fce['toFixed'](0x2)+_0x1a07('0x1')+_0x9d5602+'ml/hr';}
+
+function ketaprocess(){
+
+  const weight = document.getElementById('weight').value
+  const loadingDoseRate = document.getElementById('loadingDoseRate').value
+
+  const weightxdose = weight * loadingDoseRate
+  console.log(weightxdose)
+
+  const loadingDose = weightxdose / 100
+  var roundednumber = (Math.round(loadingDose * 100) / 100).toFixed(3);
+
+document.getElementById('result1').innerHTML = "Ketamine loading dose = " + roundednumber + " ml"  
+}
+
+function process(){
+    const weight = document.getElementById('weight').value
+    const fluidRate = document.getElementById('fluidRate').value
+    const dose_rate = document.getElementById('dose_rate').value 
+    const bagSize = document.getElementById('bagSize')
+    const bag_size = bagSize.value
+    
+    const drugConcentration = 100
+     
+    // 1. weight multiplied by fluidRate = actual Fluid Rate 
+    // An  animal  weighing  20kg needing  a  CRI  of .5mg/kg/h  
+    //  5  mg/kg/hr  ×  20  kg  =  100  mg/hour.
+    const actualFluidRate = weight * fluidRate
+    
+    //2. Dose rate needs to be in microgram not milligram -
+    //  hence real_dose_rate.
+    const realDoseRate = dose_rate / 1000
+    const stepOne = weight * realDoseRate
+
+   //3. Drug/ml fluids = the realDoseRate divided by the weight
+   const drugMlFluid = dose_rate / fluidRate
+   console.log(drugMlFluid) //
+
+   //4. Total drug = drugMlFluid times bag_size
+   const totDrug = drugMlFluid * bag_size
+
+   //5. To get the drug volume divide realDoseRate by 100
+      const drugVol = totDrug / 100
+      console.log(drugVol) //
+
+    //6. Loading Dose at 0.25mg/kg is calculated
+    // weight * 0.25 / 100
+
+    const zeroTwoFive = weight * 0.25 / 100
+
+    //7. Loading Dose at 0.5mg/kg is calculated
+    // weight * 0.5 / 100
+
+    const zeroFive = weight * 0.5 / 100
+
+     document.getElementById('result').innerHTML = "<h3>Continuous Rate Infusion Calculation <br></h3>" + 
+     "Add "+ (drugVol).toFixed(2) + "ml to the fluid bag<br> Set the fluid rate to " + actualFluidRate + "ml/hr"
+     }
